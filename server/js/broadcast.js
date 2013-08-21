765eba54e90a91bdb2362f240545900dccf486a4
Tiago Butzke <tiago.butzke@fcamara.com.br>
2013-08-21 11:42:14 -0300

    Alterando o server pra usar o novo padrão do modulo ws

--- server/js/broadcast.js
+++ server/js/broadcast.js
@@ -1,29 +1,42 @@
-var sys = require('sys'), 
-    ws = require('ws');
+var sys = require('util'), 
+    ws = require('ws').Server;
     
-var clients = [];
+var server = new ws({port: 8080});
+
+clients = [];
+
+server.on("connection", function(websocket) {
+    clients.push(ws);
+
+    websocket.on('message', function(data) {
+        for (var i = 1; i < clients.length; i++) {
+           clients[i].send(data);
+        }
+    });
+});
+
+/*var clients = [];
 
 var server = ws.createServer(
     function (websocket) {
         clients.push(websocket);
         
         websocket.addListener('connect', function (resource) {
-//            sys.debug('connect: ' + resource);
-            
+            sys.debug('connect: ' + resource);
         });
         
         websocket.addListener('data', function(data) {
             for (var i = 1; i < clients.length; i++) {
                 clients[i].write(data);
             }
-            
         });
         
         websocket.addListener('close', function() {
-//            sys.debug('close');
+            sys.debug('close');
         });
     });
     
-server.listen(9999);
+server.listen(8080);
+*/
 
-//sys.debug('Listening on port 8080');
+sys.debug('Listening on port 8080');
