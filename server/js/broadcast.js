d2e3ef2a2e27302f4d18b377acc5b70522ae655a
tiagobutzke <tiago.butzke@gmail.com>
2013-08-21 23:38:25 -0300

    improving server: disconnect websockets

--- server/js/broadcast.js
+++ server/js/broadcast.js
@@ -7,37 +7,25 @@ clients = [];
 
 server.on("connection", function(websocket) {
     clients.push(websocket);
-
+    
+    sys.debug(clients.length);
+    
     websocket.on('message', function(data) {
-        console.log(clients.length);
         for (var i = 1; i < clients.length; i++) {
            clients[i].send(data);
         }
     });
-});
-
-/*var clients = [];
-
-var server = ws.createServer(
-    function (websocket) {
-        clients.push(websocket);
-        
-        websocket.addListener('connect', function (resource) {
-            sys.debug('connect: ' + resource);
-        });
-        
-        websocket.addListener('data', function(data) {
-            for (var i = 1; i < clients.length; i++) {
-                clients[i].write(data);
-            }
-        });
-        
-        websocket.addListener('close', function() {
-            sys.debug('close');
-        });
-    });
     
-server.listen(8080);
-*/
+    websocket.on('close', function() {
+        sys.debug('close');
+        
+        for (var i = 0; i < clients.length; i++) {
+            if (clients[i] == websocket) {
+                clients.splice(i);
+                break;
+            }
+        }
+    });
+});
 
 sys.debug('Listening on port 8080');
